540bce47fde41706770596248dc722a3a1667248
Jivesh Singh <Singhjivesh57@gmail.com>
2024-04-14 20:59:07 +0530

    diff added

--- calc.c
+++ calc.c
@@ -1,13 +1,17 @@
 int main() {    
 
-    int number1, number2, sum;
+    int number3, number2, sum;
     
     printf("Enter two integers: ");
-    scanf("%d %d", &number1, &number2);
+    scanf("%d %d", &number3, &number2);
 
     // calculate the sum
-    sum = number1 + number2;      
+    sum = number3 + number2;   
+
+    // difference
+    diff =  number3 - number2;   
     
-    printf("%d + %d = %d", number1, number2, sum);
+    printf("%d + %d = %d", number3, number2, sum);
+    printf("%d - %d = %d", number3, number2, diff);
     return 0;
 }
